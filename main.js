a850b5fb9b91d6e291b0ed9ab611a1c0449f4390
Varunv1234 <98043587+Varunv1234@users.noreply.github.com>
2022-02-24 08:17:29 -0600

    1234

--- main.js
+++ main.js
@@ -1,4 +1,14 @@
-var student = "Varun Viges";
-var age = 13;
-var isEnrolled = false;
-console.log("student:" + student)
+window.onload = function() {
+
+
+let button = document.querySelector("#b");
+  button.addEventListener('click', addText);
+
+function addText() {
+  var textField = document.getElementById("h11");
+  var abc = "<h1> 1234 </h1>";
+  textField.innerHTML = abc;
+}
+
+}
+
