a8fc9147cb3f047ba6d64d550f1c923224252c75
Varunv1234 <98043587+Varunv1234@users.noreply.github.com>
2022-04-07 09:28:26 -0500

    final

--- main.js
+++ main.js
@@ -34,7 +34,25 @@ var clear5 = document.getElementById("clear5");
 var clear6= document.getElementById("clear6");
  clear6.addEventListener("click", clear16);
 
+/*
+var clear1 = document.getElementById("up1");
+ clear1.addEventListener("click", up11);
 
+var clear2 = document.getElementById("up2");
+ clear2.addEventListener("click", up12);
+
+ var clear3 = document.getElementById("up3");
+ clear3.addEventListener("click", up13);
+
+ var clear4 = document.getElementById("up4");
+ clear4.addEventListener("click", up14)
+
+var clear5 = document.getElementById("up5");
+ clear5.addEventListener("click", up15);
+
+var clear6= document.getElementById("up6");
+ clear6.addEventListener("click", up16);
+*/
 
 
 
@@ -236,7 +254,7 @@ function clear11() {
  	document.getElementById("op21").style.backgroundColor = "rgba(245, 245, 245, 0.8)";
  	document.getElementById("op22").style.backgroundColor = "rgba(245, 245, 245, 0.8)";
  	document.getElementById("op23").style.backgroundColor = "rgba(245, 245, 245, 0.8)";
-
+ return clickNumber = 2;
 
 }
 
@@ -251,6 +269,8 @@ function clear12() {
   document.getElementById("op31").style.backgroundColor = "rgba(245, 245, 245, 0.8)";
  	document.getElementById("op32").style.backgroundColor = "rgba(245, 245, 245, 0.8)";
  	document.getElementById("op33").style.backgroundColor = "rgba(245, 245, 245, 0.8)";
+ 	 return clickNumber = 3;
+
 }
 
 function clear13() {
@@ -263,6 +283,8 @@ var textField = document.getElementById("op41");
   document.getElementById("op41").style.backgroundColor = "rgba(245, 245, 245, 0.8)";
  	document.getElementById("op42").style.backgroundColor = "rgba(245, 245, 245, 0.8)";
  	document.getElementById("op43").style.backgroundColor = "rgba(245, 245, 245, 0.8)";
+ 	 return clickNumber = 4;
+
 }
 
 function clear14() {
@@ -275,6 +297,8 @@ function clear14() {
   document.getElementById("op51").style.backgroundColor = "rgba(245, 245, 245, 0.8)";
  	document.getElementById("op52").style.backgroundColor = "rgba(245, 245, 245, 0.8)";
  	document.getElementById("op53").style.backgroundColor = "rgba(245, 245, 245, 0.8)";
+ 	 return clickNumber = 5;
+
 }
 
 function clear15() {
@@ -287,6 +311,8 @@ function clear15() {
   document.getElementById("op61").style.backgroundColor = "rgba(245, 245, 245, 0.8)";
  	document.getElementById("op62").style.backgroundColor = "rgba(245, 245, 245, 0.8)";
  	document.getElementById("op63").style.backgroundColor = "rgba(245, 245, 245, 0.8)";
+ 	 return clickNumber = 6;
+
 }
 
 function clear16() {
@@ -298,8 +324,111 @@ function clear16() {
   textField3.innerHTML = '<h4> </h4>';
   document.getElementById("op71").style.backgroundColor = "rgba(245, 245, 245, 0.8)";
  	document.getElementById("op72").style.backgroundColor = "rgba(245, 245, 245, 0.8)";
- 	document.getElementById("op73").style.backgroundColor = "rgba(245, 245, 245, 0.8)";
+ 	document.getElementById("op73").style.backgroundColor = "rgba(245, 245, 245, 0.8)"; 
+ 	return clickNumber = 7;
+
 }
 
+/*
+function up11() {
+
+clear11();
+
+	var sevenObj = new Object();
+sevenObj.item = document.getElementById('op71').value;
+sevenObj.date = document.getElementById('op72').value;
+sevenObj.priority = document.getElementById('op73').value;
+
+
+var sixObj = new Object();
+sixObj.item = document.getElementById('op61').value;
+sixObj.date = document.getElementById('op62').value;
+sixObj.priority = document.getElementById('op63').value;
+sixObj.priorityInt = parseInt(sixObj.priority);
+sixObj.dateInt = parseInt(sixObj.date);
+sixObj.todayDate = todayDate.getDate();;
+sixObj.todaydateInt = parseInt(sixObj.todayDate);
+sixObj.clickNumber = clickNumber;
+sixObj.rank = sixObj.priorityInt/(sixObj.dateInt - sixObj.todaydateInt) * 1/3;
+
+var fiveObj = new Object();
+fiveObj.item = document.getElementById('op51').value;
+fiveObj.date = document.getElementById('op52').value;
+fiveObj.priority = document.getElementById('op53').value;
+fiveObj.priorityInt = parseInt(fiveObj.priority);
+fiveObj.dateInt = parseInt(fiveObj.date);
+fiveObj.todayDate = todayDate.getDate();;
+fiveObj.todaydateInt = parseInt(fiveObj.todayDate);
+fiveObj.clickNumber = clickNumber;
+fiveObj.rank = fiveObj.priorityInt/(fiveObj.dateInt - fiveObj.todaydateInt) * 1/3;
+
+
+var fourObj = new Object();
+fourObj.item = document.getElementById('op41').value;
+fourObj.date = document.getElementById('op42').value;
+fourObj.priority = document.getElementById('op43').value;
+fourObj.priorityInt = parseInt(fourObj.priority);
+fourObj.dateInt = parseInt(fourObj.date);
+fourObj.todayDate = todayDate.getDate();;
+fourObj.todaydateInt = parseInt(fourObj.todayDate);
+fourObj.clickNumber = clickNumber;
+fourObj.rank = fourObj.priorityInt/(fourObj.dateInt - fourObj.todaydateInt) * 1/3;
+
+
+var threeObj = new Object();
+threeObj.item = document.getElementById('op31').value;
+threeObj.date = document.getElementById('op32').value;
+threeObj.priority = document.getElementById('op33').value;
+threeObj.priorityInt = parseInt(threeObj.priority);
+threeObj.dateInt = parseInt(threeObj.date);
+threeObj.todayDate = todayDate.getDate();;
+threeObj.todaydateInt = parseInt(threeObj.todayDate);
+threeObj.clickNumber = clickNumber;
+threeObj.rank = threeObj.priorityInt/(threeObj.dateInt - threeObj.todaydateInt) * 1/3;
+
+var twoObj = new Object();
+twoObj.item = document.getElementById('op21').value;
+twoObj.date = document.getElementById('op22').value;
+twoObj.priority = document.getElementById('op23').value;
+twoObj.priorityInt = parseInt(twoObj.priority);
+twoObj.dateInt = parseInt(twoObj.date);
+twoObj.todayDate = todayDate.getDate();;
+twoObj.todaydateInt = parseInt(twoObj.todayDate);
+twoObj.clickNumber = clickNumber;
+twoObj.rank = twoObj.priorityInt/(twoObj.dateInt - twoObj.todaydateInt) * 1/3;
+
+console.log(sevenObj);
+console.log(twoObj);
+	var textField = document.getElementById("op21");
+  textField.innerHTML = '<h4>' + threeObj.item + '</h4>';
+  var textField2 = document.getElementById("op22");
+  textField2.innerHTML = '<h4>' + threeObj.date + '</h4>';
+  var textField3 = document.getElementById("op23");
+  textField3.innerHTML = '<h4>' + threeObj.priority + '</h4>';
+
+  	var textField = document.getElementById("op31");
+  textField.innerHTML = '<h4>' + fourObj.item + '</h4>';
+  var textField2 = document.getElementById("op32");
+  textField2.innerHTML = '<h4>' + fourObj.date + '</h4>';
+  var textField3 = document.getElementById("op33");
+  textField3.innerHTML = '<h4>' + fourObj.priority + '</h4>';
+
+    	var textField = document.getElementById("op51");
+  textField.innerHTML = '<h4>' + sixObj.item + '</h4>';
+  var textField2 = document.getElementById("op52");
+  textField2.innerHTML = '<h4>' + sixObj.date + '</h4>';
+  var textField3 = document.getElementById("op53");
+  textField3.innerHTML = '<h4>' + sixObj.priority + '</h4>';
+
+  var textField = document.getElementById("op61");
+  textField.innerHTML = '<h4>' + sevenObj.item + '</h4>';
+  var textField2 = document.getElementById("op62");
+  textField2.innerHTML = '<h4>' + sevenObj.date + '</h4>';
+  var textField3 = document.getElementById("op63");
+  textField3.innerHTML = '<h4>' + sevenObj.priority + '</h4>';
+
+
+}
+*/
 }
 
